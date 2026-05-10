ca9b923674691c22797f0862dea033973bd0a4ef
WassimEl <welhadji@francoisdesales.be>
2026-05-10 02:18:52 +0200

    Nouvel article

--- publier.bat
+++ publier.bat
@@ -1,11 +1,23 @@
 @echo off
-d:
 cd /d "D:\Obsidian coffres\quartz"
+
+echo 🔨 1/5 - Génération du site...
 npx quartz build
+
+echo 📁 2/5 - Préparation du dossier docs...
 if exist docs rmdir /s /q docs
 rename public docs
+
+echo 📤 3/5 - Ajout des fichiers modifiés...
 git add -A
-git commit -m "Nouvel article - %date%"
+
+echo 💾 4/5 - Enregistrement des changements...
+git commit -m "Nouvel article - %date% %time%"
+
+echo 🚀 5/5 - Envoi vers GitHub...
 git push origin v4
-echo ✅ Article publié ! https://wassimel.github.io/quartz/index.xml
+
+echo.
+echo ✅ Terminé ! Flux RSS : https://wassimel.github.io/quartz/index.xml
+echo.
 pause
